20868c037b0875e47e355265a23d6fd30e3fd7e2
Farid <farid.adinani@hotmail.com>
2025-05-27 11:56:09 +0200

    personnalisation de la partie Tweet avec changement de couleur si nbr caractère supperieur à 280

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../styles/Tweet.module.css";
 
 export default function Tweet() {
+
+    const [tweet, setTweet] = useState('')
+    let maxCarac = tweet.length > 280 ? {color:'red'} : {color:'white'}
+
+
   return (
-    <div>
-      Tweet
-      <div>
-        <input type="text" id="tweet" name="tweet" placeholder="What's Up ?" />
+    <div className={styles.containerTweet}>
+      <div className={styles.input}>
+        <input
+          className={styles.inputTweet}
+          type="text"
+          id="tweet"
+          name="tweet"
+          placeholder="What's Up ?"
+          onChange={(e) => setTweet(e.target.value)}
+          value={tweet}
+        />
+      </div>
+      <div className={styles.validatonTweet}>
+        <p style={maxCarac}>{tweet.length}/280</p>
+        <button className={styles.buttonTweet}>Tweet</button>
       </div>
     </div>
   );

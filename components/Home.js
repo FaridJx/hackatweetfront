198d2bff18c77aaebfb8799af62094d4aceef96b
Farid <farid.adinani@hotmail.com>
2025-05-27 09:09:14 +0200

    demarrage du projet hackatweet

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,39 @@
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+import Tweet from './Tweet';
 
 function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        {/* <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h1> */}
+        <div id='left' className={styles.left}>
+          <div className={styles.leftContainer}>
+            <div><img
+                className={styles.leftHeader}
+                src="/assets/Logo_of_Twitter.svg.png"
+                alt="Twitter logo"
+              /></div>
+            <div className={styles.leftFooter}>
+              <div><img/></div>
+              <div>
+                <h4>John</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id='center' className={styles.center}>
+          <div className={styles.centerHeader}>
+            <h3>Home</h3>
+            <Tweet/>
+          </div>
+          <div>
+
+          </div>
+        </div>
+        <div id='right' className={styles.right}></div>
       </main>
     </div>
   );
---
+++ components/Tweet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Tweet() {
+  return (
+    <div>
+      Tweet
+      <div>
+        <input type="text" id="tweet" name="tweet" placeholder="What's Up ?" />
+      </div>
+    </div>
+  );
+}

aa70d4334c94b0fedb1ee01a61e1398a9b4b0900
Farid <farid.adinani@hotmail.com>
2025-05-27 18:19:40 +0200

    mise en page partie Tweets

--- components/LastTweets.js
+++ components/LastTweets.js
@@ -1,53 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../styles/LastTweets.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function LastTweets() {
-  const data = [
-    {
-      name: "Fani",
-      tag: "@LovyDeLaVida",
-      time: "3 minutes",
-      msg: "Lorem ipsum dolor sit amet",
-    },
-    {
-      name: "Fariha",
-      tag: "@majestyDeLaNoche",
-      time: "12 hours",
-      msg: "Lorem ipsum dolor sit amet dolor sit amet dolor sit amet",
-    },
-    {
-      name: "Fasouspi",
-      tag: "@adalynoDeNintendo",
-      time: "5 days",
-      msg: "Lorem ipsum dolor sit amet",
-    },
-  ];
+
+    // const [like, setLike] = useState(false)
+    // let style = like ? style={color: "red"} : {color: "white"}
+    const data = [
+        {
+        name: "Fani",
+        tag: "@LovyDeLaVida",
+        time: "3 minutes",
+        msg: "Lorem ipsum dolor sit amet",
+        },
+        {
+        name: "Fariha",
+        tag: "@majestyDeLaNoche",
+        time: "12 hours",
+        msg: "Lorem ipsum dolor sit amet dolor sit amet dolor sit amet",
+        },
+        {
+        name: "Fasouspi",
+        tag: "@adalynoDeNintendo",
+        time: "5 days",
+        msg: "Lorem ipsum dolor sit amet",
+        },
+    ];
 
   const tweets = data.map((e) => {
-    return (<div>
-      <div>
-        <img src="../assets/avataaars.png" alt="avatar girl" />
-        <p>{e.name}</p>
-        <p>{e.tag}</p> - <p>{e.time}</p>
-      </div>
-      <div>{e.msg}</div>
-      <div>
-        <div>
-          <FontAwesomeIcon icon={faHeart} />
-          <p>2</p>
+    const [like, setLike] = useState(false)
+    let style = like ? style={color: "red"} : {color: "white"}
+
+    return (
+      <div className={styles.tweetContainer} >
+        <div className={styles.userInfo}>
+          <img
+            className={styles.imgAvatar}
+            src="../assets/avataaars.png"
+            alt="avatar girl"
+          />
+          <p className={styles.userName}>{e.name}</p>
+          <p className={styles.userTag}>{e.tag}</p> <p>- {e.time}</p>
         </div>
-        <div>
-        <FontAwesomeIcon icon={faTrash} />
+        <div className={styles.tweetMsg}>{e.msg}</div>
+        <div className={styles.judge}>
+          <div className={styles.likeContainer}>
+            <FontAwesomeIcon icon={faHeart} className={styles.likes} style={style} onClick={() => setLike(!like)}/>
+            <p>2</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faTrash} className={styles.trash}/>
+          </div>
         </div>
       </div>
-    </div>);
+    );
   });
 
-  return (
-  <div>
-
-    {tweets}
-  </div>
-  );
+  return <div>{tweets}</div>;
 }

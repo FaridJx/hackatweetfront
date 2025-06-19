a805f811e9f702e663639662fc9ec67f9897b7e3
Farid <farid.adinani@hotmail.com>
2025-06-19 11:30:06 +0200

    creation objet pour recuperer les hashtag et compter le nombre utilisation

--- components/Trends.js
+++ components/Trends.js
@@ -4,24 +4,28 @@ import styles from '../styles/Trends.module.css'
 export default function Trends(props) {
 
     const [trends, setTrends] = useState([])
+    const objets = {}
 
-    useEffect(() => {
-        fetch("http://localhost:3000/trends")
+     useEffect( () =>{
+      fetch("http://localhost:3000/trends")
           .then((response) => response.json())
           .then((data) => {
             if (data) {
               setTrends(data);
             }
           });
-      }, [props.refresh]);
+      }, [props.onRefresh]);
 
-
-
-    const hastagList = trends.map((e, key) => {
+      for (let i = 0; i < trends.length; i++) {
+                const tweet = trends[i].hashtag;
+                objets[tweet] ? objets[tweet]+=1 : objets[tweet]=1            
+      }
+      
+      const hastagList = Object.keys(objets).map((e, key) => {
         return (
             <div>
-                <h4>#{e.hastag}</h4>
-                <p>2 Tweets</p>
+                <h4>{e}</h4>
+                <p>{objets[e]} Tweets</p>
             </div>
         )
     })

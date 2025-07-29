ca50b0ebc16441470295482659c1b8a1d3c73075
Farid <farid.adinani@hotmail.com>
2025-07-29 23:01:42 +0200

    ajout du persistor

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,33 @@
-import '../styles/globals.css';
-import Head from 'next/head';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import users from '../reducers/users';
+import "../styles/globals.css";
+import Head from "next/head";
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import users from "../reducers/users";
+
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+
+const reducers = combineReducers({ users });
+const persistConfig = { key: "hackatweet", storage };
 
 const store = configureStore({
- reducer: {users},
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
-function App({ Component, pageProps }) {
+const persistor = persistStore(store);
 
-  
+function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Head>
-        <title>Hackatweet</title>
-      </Head>
-      <Component {...pageProps} />
+      <PersistGate persistor={persistor}>
+        <Head>
+          <title>Hackatweet</title>
+        </Head>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
